slyrus/abcl

commit fe39383d028bad559b7533dd5f1dfd41cb7b13b4

Little combination fix for pool-long and pool-double.
git-svn-id: http://abcl.org/svn/trunk/abcl@11792 1c010e3e-69d0-11dd-93a8-456734b0d56f

src/org/armedbear/lisp/compiler-pass2.lisp
@@ -134,14 +134,8 @@
   (declare (optimize speed))
   (pool-get (list 4 (%float-bits n))))
 
-(defknown pool-long (integer) (integer 1 65535))
-(defun pool-long (n)
-  (declare (optimize speed))
-  (declare (type java-long n))
-  (let* ((entry (list 5
-                      (logand (ash n -32) #xffffffff)
-                      (logand n #xffffffff)))
-         (ht *pool-entries*)
+(defun pool-long/double (n entry)
+  (let* ((ht *pool-entries*)
          (index (gethash1 entry ht)))
     (declare (type hash-table ht))
     (unless index
@@ -157,28 +151,23 @@
       (setf *pool-count* (+ index 2)))
     index))
 
+(defknown pool-long (integer) (integer 1 65535))
+(defun pool-long (n)
+  (declare (optimize speed))
+  (declare (type java-long n))
+  (let* ((entry (list 5
+                      (logand (ash n -32) #xffffffff)
+                      (logand n #xffffffff))))
+    (pool-long/double n entry)))
+
 (defknown pool-double (double-float) (integer 1 65535))
 (defun pool-double (n)
   (declare (optimize speed))
   (let* ((n (%float-bits n))
          (entry (list 6
                       (logand (ash n -32) #xffffffff)
-                      (logand n #xffffffff)))
-         (ht *pool-entries*)
-         (index (gethash1 entry ht)))
-    (declare (type hash-table ht))
-    (unless index
-      (setf index *pool-count*)
-      (push entry *pool*)
-      (setf (gethash entry ht) index)
-      ;; The Java Virtual Machine Specification, Section 4.4.5: "All 8-byte
-      ;; constants take up two entries in the constant_pool table of the class
-      ;; file. If a CONSTANT_Long_info or CONSTANT_Double_info structure is the
-      ;; item in the constant_pool table at index n, then the next usable item in
-      ;; the pool is located at index n+2. The constant_pool index n+1 must be
-      ;; valid but is considered unusable." So:
-      (setf *pool-count* (+ index 2)))
-    index))
+                      (logand n #xffffffff))))
+    (pool-long/double n entry)))
 
 (defknown u2 (fixnum) cons)
 (defun u2 (n)

commit 5c62b106f28d977c1365b352fb81a6a95ef34ce0

Ant-based build works again, fixing error introduced in r11792
git-svn-id: http://abcl.org/svn/trunk/abcl@11795 1c010e3e-69d0-11dd-93a8-456734b0d56f

src/org/armedbear/lisp/compiler-pass2.lisp
@@ -134,7 +134,7 @@
   (declare (optimize speed))
   (pool-get (list 4 (%float-bits n))))
 
-(defun pool-long/double (n entry)
+(defun pool-long/double (entry)
   (let* ((ht *pool-entries*)
          (index (gethash1 entry ht)))
     (declare (type hash-table ht))
@@ -158,7 +158,7 @@
   (let* ((entry (list 5
                       (logand (ash n -32) #xffffffff)
                       (logand n #xffffffff))))
-    (pool-long/double n entry)))
+    (pool-long/double entry)))
 
 (defknown pool-double (double-float) (integer 1 65535))
 (defun pool-double (n)
@@ -167,7 +167,7 @@
          (entry (list 6
                       (logand (ash n -32) #xffffffff)
                       (logand n #xffffffff))))
-    (pool-long/double n entry)))
+    (pool-long/double entry)))
 
 (defknown u2 (fixnum) cons)
 (defun u2 (n)