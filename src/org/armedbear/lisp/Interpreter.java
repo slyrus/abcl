slyrus/abcl

commit eafa3484e429694a49671dd6007e6be332111ab5

Load 'system.lisp' later in boot so conditions trigger debugger.
git-svn-id: http://abcl.org/svn/trunk/abcl@12673 1c010e3e-69d0-11dd-93a8-456734b0d56f

src/org/armedbear/lisp/Interpreter.java
@@ -92,6 +92,7 @@ public final class Interpreter
         }
         initializeLisp();
         initializeTopLevel();
+        initializeSystem();
         if (!noinit)
             processInitializationFile();
         if (args != null)
@@ -117,6 +118,7 @@ public final class Interpreter
 
         initializeJLisp();
         initializeTopLevel();
+        initializeSystem();
         processInitializationFile();
         return interpreter;
     }
@@ -209,6 +211,11 @@ public final class Interpreter
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static synchronized void initializeSystem() 
+    {
+        Load.loadSystemFile("system");
     }
 
     // Check for --noinit; verify that arguments are supplied for --load and

src/org/armedbear/lisp/boot.lisp
@@ -209,12 +209,4 @@
     (%format t "Startup completed in ~A seconds.~%"
              (float (/ (ext:uptime) 1000)))))
 
-;;; "system.lisp" contains system installation specific information
-;;; (currently only the logical pathname definition for "SYS;SRC")
-;;; that is not currently required for ABCL to run.  Since
-;;; LOAD-SYSTEM-FILE exits the JVM if its argument cannot be found, we
-;;; use REQUIRE trapping any error.
-(handler-case 
-    (require 'system)
-  (t ()))
 

commit 0ac8fe67eb1874c1190ee66171fbe8daa27963fe

Fix build from scratch breakage in r12673.
git-svn-id: http://abcl.org/svn/trunk/abcl@12675 1c010e3e-69d0-11dd-93a8-456734b0d56f

build.xml
@@ -241,6 +241,7 @@ to   ${abcl.lisp.output}</echo>
 	    classname="org.armedbear.lisp.Main">
         <jvmarg value="-Dabcl.home=${abcl.home.dir}${file.separator}"/>
 	<arg value="--noinit"/>
+        <arg value="--nosystem"/>
         <arg value="--eval"/>
         <arg value="(setf *load-verbose* t)"/>
       </java>
@@ -278,12 +279,19 @@ to   ${abcl.lisp.output}</echo>
     </target>
 
     <target name="abcl.system.uptodate">
-      <uptodate property="abcl.system.needs-update.p" 
+      <condition property="abcl.system.needs-update.p">
+        <and>
+          <available file="${system.lisp.file}"/>
+          <available file="${abcl.startup.file}"/>
+          <uptodate
                 srcfile="${system.lisp.file}"
                 targetfile="${abcl.startup.file}"/>
+        </and>
+      </condition>
     </target>
     
-    <target name="abcl.system.update.maybe" depends="abcl.system.uptodate" if="abcl.system.needs-update.p">
+    <target name="abcl.system.update.maybe" depends="abcl.system.uptodate" 
+            if="abcl.system.needs-update.p">
       <touch file="${src.dir}/org/armedbear/lisp/compile-system.lisp"/>
     </target>
 

src/org/armedbear/lisp/Interpreter.java
@@ -52,6 +52,7 @@ public final class Interpreter
     private final OutputStream outputStream;
 
     private static boolean noinit = false;
+    private static boolean nosystem = false;
     private static boolean noinform = false;
 
     public static synchronized Interpreter getInstance()
@@ -92,7 +93,8 @@ public final class Interpreter
         }
         initializeLisp();
         initializeTopLevel();
-        initializeSystem();
+        if (!nosystem) 
+            initializeSystem();
         if (!noinit)
             processInitializationFile();
         if (args != null)
@@ -231,6 +233,8 @@ public final class Interpreter
                 String arg = args[i];
                 if (arg.equals("--noinit")) {
                     noinit = true;
+                } else if (arg.equals("--nosystem")) {
+                    nosystem = true;
                 } else if (arg.equals("--noinform")) {
                     noinform = true;
                 } else if (arg.equals("--batch")) {